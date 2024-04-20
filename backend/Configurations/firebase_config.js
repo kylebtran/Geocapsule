6c3ee6160bff43db2e4b7df78fde0bff1e3d00fc
Alina Sheikh <sheikh.alina@gmail.com>
2024-04-20 15:19:42 -0700

    fixed gemini prompts

--- backend/Configurations/firebase_config.js
+++ backend/Configurations/firebase_config.js
@@ -11,4 +11,3 @@ const firebaseConfig = {
 };
 
 export default firebaseConfig;
-
--- backend/Controllers/getting-prompts.js
+++ backend/Controllers/getting-prompts.js
@@ -2,17 +2,17 @@ import express, { Router } from "express";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 const router = express.Router();
 
-let API_KEY = "AIzaSyCrsdyjOBQJfzGmRrtFyynWRYBd0bVrKZs";
+let API_KEY = "AIzaSyA4LpEp8-U7TLPT186h2vIbdbLgzjG_UZg";
 
 // Starting the genAI
 const genAI = new GoogleGenerativeAI(API_KEY);
 
 // Model for the text input
-const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+const model = genAI.getGenerativeModel({ model: "gemini-pro", 
+        system_instruction:"Start each prompt with a verb, and return fun & safe prompts. Never use the word \"let's\" and don't use emojis. Be lighthearted."});
 
 async function run() {
-    const prompt = "Give 3 prompts for people to take pictures together: one should be generic, and two should be in and around UCLA. Don't use a label of which prompt it is."
-  
+    const prompt = "Give 3 prompts for people to take pictures together: one should be generic, and two should be in and around UCLA. Don't use a label of which prompt it is. Make the generic prompt describe doing an activity outside."
     const result = await model.generateContent(prompt);
     const response = await result.response;
     const text = response.text();
@@ -30,4 +30,3 @@ router.get('/', async(req, res) => {
 })
 
 export default router;
-
